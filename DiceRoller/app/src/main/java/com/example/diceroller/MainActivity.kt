705f7311a005ac1a75aaf89e96228d29f9cc6bdf
Sean Owen <sean_owen@live.co.uk>
2022-09-29 10:07:04 -0700

    finish dice roller guide

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var rollButton: Button = findViewById(R.id.button);
+        rollButton.setOnClickListener { rollDice(); };
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        val diceRoll = Dice(6).roll();
+        val resultTextView: TextView = findViewById(R.id.textView);
+        resultTextView.text = diceRoll.toString();
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random();
     }
 }
